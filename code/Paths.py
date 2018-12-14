6dce60dec120208500620c9c2dafbf961e91501f
Roberto P. Romero <Roberto.Plaza@alu.uclm.es>
2018-12-14 18:19:37 +0100

    Fixed somethings

--- code/Paths.py
+++
@@ -1,3 +0,0 @@
-user_interactions   =   'data/cursos_modulo.csv'
-last_interaction    =   'data/ultima_interaccion_usuarios_modulo.csv'
-total_interaction   =   'data/total_interacciones_usuario_modulo.csv'
--- src/Preprocessing.py
+++ src/Preprocessing.py
@@ -1,11 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 import pandas as pd
 import numpy
 
-ruta_cursos_iteracciones_modulo =   'data/cursos_modulo.csv'
-ruta_cursos_iteracciones_modulo_preprocesado = 'data/cursos_modulo_preprocesado.csv'
+import Paths
 
 def read_dataset(path_no_attacks):
     return pd.read_csv(path_no_attacks)
@@ -23,12 +22,12 @@ def clean_first_line_dataset(path_in,path_out):
         word_list = line.split(',')
         attrib = ', '.join(word_list[1:])
         output_file.write(attrib)
-        #output_file.write('\n')
         line=input_file.readline()
+
     output_file.close()
 
 if __name__ == '__main__':
-    a=read_dataset(ruta_cursos_iteracciones_modulo)
+    a=read_dataset(Paths.user_interactions)
     a.drop(a.columns[[0]], axis=1, inplace=True)
-    clean_first_line_dataset(ruta_cursos_iteracciones_modulo,ruta_cursos_iteracciones_modulo_preprocesado)
+    clean_first_line_dataset(Paths.user_interactions, Paths.preprocessed_interactions)
     
