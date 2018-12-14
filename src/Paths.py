782e539a41ff60c6802b9a8a7b65b9c57ef3cba2
Roberto P. Romero <Roberto.Plaza@alu.uclm.es>
2018-12-14 18:22:12 +0100

    Fixed some things

--- src/Paths.py
+++ src/Paths.py
@@ -1,4 +1,4 @@
-user_interactions           =   'data/cursos_modulo.csv'
-preprocessed_interactions   =   'data/cursos_modulo_preprocesado.csv'
-last_interaction            =   'data/ultima_interaccion_usuarios_modulo.csv'
-total_interaction           =   'data/total_interacciones_usuario_modulo.csv'
+user_interactions           =   '../data/cursos_modulo.csv'
+preprocessed_interactions   =   '../data/cursos_modulo_preprocesado.csv'
+last_interaction            =   '../data/ultima_interaccion_usuarios_modulo.csv'
+total_interaction           =   '../data/total_interacciones_usuario_modulo.csv'
