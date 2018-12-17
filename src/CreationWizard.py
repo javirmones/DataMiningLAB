f414f71404ddbbef0cec97b8f65c755c78230f5d
Alf <alfonso.barragan@alu.uclm.es>
2018-12-17 19:36:10 +0100

    So close to ending this epoch of preprocessing

--- src/CreationWizard.py
+++ src/CreationWizard.py
@@ -19,7 +19,7 @@ class CreationWizard():
         self.db = DatabaseKDD()
 
     def create_dataframes(self):
-        '''
+
         module_last_activity_query              = self.db.query("SELECT DISTINCT(t1.enrollment_id), t1.object, MAX(t1.time) AS time, t2.username, t2.course_id\nFROM log_train AS t1\nJOIN enrollment_train AS t2 on t1.enrollment_id = t2.enrollment_id\nGROUP BY t1.enrollment_id\nORDER BY t1.enrollment_id;")
         module_last_activity_list               = [list(enrollment_id) for enrollment_id in module_last_activity_query]
         write_from_list("enrollment_id,object,time,username,course\n", module_last_activity_list, route_of.last_interaction)                        
@@ -28,13 +28,13 @@ class CreationWizard():
         course_module_list                      = [list(course_id) for course_id in course_module_query]
         write_from_list("module_id,course_id\n", course_module_list, route_of.modules_by_course)
         
-        module_activity_query                   = self.db.query("SELECT DISTINCT(t1.enrollment_id), t1.object, COUNT(t1.object) AS interactions, t1.event AS event, t1.source AS source, t2.course_id, t2.username\nFROM log_train AS t1\nJOIN enrollment_train AS t2 on t1.enrollment_id = t2.enrollment_id\nGROUP BY t1.object\nORDER BY t1.enrollment_id;")
-        module_activity_list                    = [list(enrollment_id) for enrollment_id in module_activity_query]
-        write_from_list("enrollment_id,object,interactions,event,source,course_id,username\n", module_activity_list, route_of.total_interaction)        
+#        module_activity_query                   = self.db.query("SELECT DISTINCT(t1.enrollment_id), t1.object, COUNT(t1.object) AS interactions, t1.event AS event, t1.source AS source, t2.course_id, t2.username\nFROM log_train AS t1\nJOIN enrollment_train AS t2 on t1.enrollment_id = t2.enrollment_id\nGROUP BY t1.object\nORDER BY t1.enrollment_id;")
+#        module_activity_list                    = [list(enrollment_id) for enrollment_id in module_activity_query]
+#        write_from_list("enrollment_id,object,interactions,event,source,course_id,username\n", module_activity_list, route_of.total_interaction)        
 
         target_data_course_submodules_query     = self.db.query("SELECT course_id, COUNT(module_id) AS n_submodules\nFROM objets2\nGROUP BY course_id")
         target_data_course_submodules_list      = [list(course_id) for course_id in target_data_course_submodules_query]
-        print(target_data_course_submodules_list)
+        write_from_list("course_id, n_submodules_total\n", target_data_course_submodules_list, route_of.number_submodules_by_courses)
         
         categories_query        = self.db.query("SELECT DISTINCT(category)\nFROM objets")
         
@@ -42,11 +42,7 @@ class CreationWizard():
             target_data_category_module_query   = self.db.query("SELECT course_id, COUNT(category) AS number_category\nFROM objets2\nWHERE category='{}'\nGROUP BY course_id".format(category[0]))
             target_data_category_module_list    = [list(course_id) for course_id in target_data_category_module_query]
             write_from_list("course_id, number_of_{}\n".format(category[0]), target_data_category_module_list, route_of.target_data_category.format(category[0]))
-    '''
-        dates_per_course_query                  = self.db.query("SELECT o.course_id, d.from, d.to FROM objets AS o JOIN date AS d ON d.course_id = o.course_id")
-        dates_per_course_list                   = [list(course_dates) for course_dates in dates_per_course_query]
-        write_from_list("course_id,course_begin,course_end\n", dates_per_course_list, route_of.courses_dates) 
-        
+            print(target_data_category_module_list)
         # TO DO: numero usuarios por curso, tipos de fuente por curso, usuarios abandono, usuarios no abandono
         users_per_course_query                  = self.db.query("SELECT course_id, COUNT(username) FROM enrollment_train GROUP BY course_id")
         users_per_course_list                   = [list(course_users) for course_users in users_per_course_query]
--- src/Preprocessing.py
+++ src/Preprocessing.py
@@ -54,16 +54,70 @@ def clean_first_line_dataset(path_in,path_out):
       #output_file.write('\n')
       line=input_file.readline()
   output_file.close()
+  
+def calculate_dropouts_and_finished_by_course(df_labeled, df_courses):
+    
+    df_courses['n_drops']       = 0
+    df_courses['n_finished']    = 0
+    
+    for course in df_courses['course_id']:
+        df_drops  = df_labeled[(df_labeled.course_id == course) & (df_labeled.finished_course == 0)]
+        df_finish = df_labeled[(df_labeled.course_id == course) & (df_labeled.finished_course == 1)]
+        
+        index = df_courses[(df_courses.course_id == course)].index
+        df_courses.iloc[index, 3] = len(df_drops)
+        df_courses.iloc[index, 4] = len(df_finish)
+  
+
+def assign_number_submodules_by_type_and_course(category_list, df_course):
+    
+    for category in category_list:
+        df_category     = read_dataset(route_of.target_data_category.format(category))
+        
+        for course in df_course['course_id']:
+            if (len(df_category[(df_category.course_id == course)])) <= 0:
+                aux = pd.DataFrame([[course, 0]], [len(df_category)], ['course_id', 'number_of_{}'.format(category)])
+                df_category = pd.concat([df_category,aux])
+        
+        to_csv(route_of.target_data_category.format(category), df_category)
+        
+def combine_datasets_to_target_data(categories):
+    
+    data_target_users_per_course        = read_dataset(route_of.users_per_course)
+    data_target_courses_date            = read_dataset(route_of.date_courses)
+    data_target_submodules_total        = read_dataset(route_of.number_submodules_by_courses)
+    
+    data_target_2   = data_target_users_per_course.merge(data_target_courses_date, on='course_id')
+    data_target_3   = data_target_2.merge(data_target_submodules_total, on='course_id')
+    
+    for category in categories:
+        df_category     = read_dataset(route_of.target_data_category.format(category))
+        data_target_3   = data_target_3.merge(df_category, on='course_id')
+
+    return data_target_3
+
+    # TO DO CONTAR USUARIOS ABANDONO EN METODO APARTE Y UNIR TODOS LOS DATASETS
 
 if __name__ == '__main__':
   
-  fechas_curso                  = read_dataset(route_of.date_courses)
-  ultima_interaccion_cursos     = read_dataset(route_of.last_interaction)
-  cursos_usuario                = read_dataset(route_of.enrollment_data)
-  interacciones_usuario         = read_dataset(route_of.data_interactions)
-  interacciones_usuario_module  = read_dataset(route_of.total_interaction)
+  categories = ['about', 'chapter', 'course', 'course_info', 'html',
+                  'outlink', 'problem', 'sequential', 'static_tab',
+                  'vertical', 'video', 'combinedopenended', 'peergrading',
+                  'discussion', 'dictation']
+#  fechas_curso                  = read_dataset(route_of.date_courses)
+#  ultima_interaccion_cursos     = read_dataset(route_of.last_interaction)
+#  cursos_usuario                = read_dataset(route_of.enrollment_data)
+#  interacciones_usuario         = read_dataset(route_of.data_interactions)
+#  interacciones_usuario_module  = read_dataset(route_of.total_interaction)
+#  
+#  log_enrollment                = cursos_usuario.merge(interacciones_usuario, on='enrollment_id')  
+#  
+#  labeling_dropouts(log_enrollment, fechas_curso, cursos_usuario)
+#  calculate_dropouts_and_finished_by_course(cursos_usuario, fechas_curso)
+#  
+#  to_csv(route_of.labeling_dropout, cursos_usuario)
+#  to_csv(route_of.users_drop_and_finished_by_course, fechas_curso)
   
-  log_enrollment                = cursos_usuario.merge(interacciones_usuario, on='enrollment_id')  
-  to_csv(route_of.labeling_dropout, cursos_usuario)
+  assign_number_submodules_by_type_and_course(categories, fechas_curso)
+  data_target = combine_datasets_to_target_data(categories)
   
-  labeling_dropouts(log_enrollment, fechas_curso, cursos_usuario)
