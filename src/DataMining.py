c1510fa2dba67432721c0c770fac720a71fb9da6
Alf <alfonso.barragan@alu.uclm.es>
2018-12-18 15:26:24 +0100

    keep the mining up

--- src/DataMining.py
+++ src/DataMining.py
@@ -2,11 +2,13 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 import numpy
+from numpy import corrcoef, transpose, arange
 import datetime as dt
 import Paths as route_of
 import matplotlib.pyplot as plt
 
 import sklearn.neighbors
+import seaborn as sns
 
 from sklearn import metrics
 from sklearn.cluster import KMeans, DBSCAN
@@ -15,13 +17,14 @@ from sklearn.decomposition import PCA
 from pylab import pcolor, xticks, yticks
 from sklearn import preprocessing
 
+
 def plot_courses(data_target):
     data_to_plot = data_target.sort_values('dropout_percentage')
     plt.plot(data_to_plot['dropout_percentage'])
 
 def normalize_filtered_data(file):
 
-    exclude         = ['course_id','from','to']
+    exclude         = ['course_id','from','to','course','course_info','combinedopened','n_navigates','n_discussions','n_wikis','n_page_close','duration_in_days']
     df_ex           = file.loc[:, file.columns.difference(exclude)]
 
     min_max_scaler  = preprocessing.MinMaxScaler()
@@ -34,10 +37,9 @@ def test_corr(df_ex):
     R   = corrcoef(transpose(df_ex))
     pcolor(R)
     plt.colorbar()
-    yticks(arange(0,14),range(0,14))
-    xticks(arange(0,14),range(0,14))
-    plt.savefig("plots/correlation.png")
-
+    yticks(arange(0,19),range(0,19))
+    xticks(arange(0,19),range(0,19))
+    plt.show()
     
     sns.set(style="white")
     mask = numpy.zeros_like(R, dtype=numpy.bool)
@@ -55,7 +57,7 @@ def test_corr(df_ex):
                 square=True, xticklabels=2, yticklabels=2,
                 linewidths=.5, cbar_kws={"shrink": .5}, ax=ax)
     
-    plt.savefig("plots/heat_map.png")
+    plt.show()
     
 
 
@@ -67,7 +69,7 @@ def calculatePCA(dataframe):
     #Print
     print(estimator.explained_variance_ratio_)
     pd.DataFrame(numpy.matrix.transpose(estimator.components_),
-    columns=['PC-1', 'PC-2'], index=dataframe.columns)
+columns=['PC-1', 'PC-2'], index=dataframe.columns)
 
     #Print
     fig, ax = plt.subplots()
@@ -81,6 +83,20 @@ def calculatePCA(dataframe):
     fig.tight_layout()
     return X_pca
 
+def plot_pca(X_pca, labels, type_clus):
+
+    colors      = numpy.array([x for x in 'bgrcmykbgrcmykbgrcmykbgrcmyk'])
+    colors      = numpy.hstack([colors] * 20)
+    fig, ax     = plt.subplots()
+
+    for i in range(len(X_pca)):
+        plt.text(X_pca[i][0], X_pca[i][1], '.', color=colors[labels[i]])
+
+    plt.xlim(-1, 1)
+    plt.ylim(-0.5, 0.8)
+    ax.grid(True)
+    fig.tight_layout()
+    
     
 def read_dataset(path):
     return pd.read_csv(path)
@@ -90,6 +106,13 @@ if __name__ == '__main__':
     
     data_target_treated = data_target.drop(data_target.columns[[0]], axis=1) 
     data_norm = normalize_filtered_data(data_target_treated)
+    data_norm = pd.DataFrame(data_norm)
     
-    calculatePCA(data_norm)
-    plot_courses(data_target)
+#    calculatePCA(data_norm)
+#    plot_courses(data_target)
+    test_corr(data_norm)
+    # Da rangos, hay que cambiarlos a etiqueta
+    data_target_treated['group'] = pd.cut(data_target_treated['dropout_percentage'], 3)
+    
+    
+    
